be94f65556dd63e77bfb9563e797b4186e6b43cc
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-03-20 15:38:51 +0000

    update front page

--- src/app/page.js
+++ src/app/page.js
@@ -14,40 +14,28 @@ export default function HomePage() {
 
       <CarouselComponent />
 
-      {/* Featured Recipes Section */}
-      <section className="bg-secondary py-16">
+      {/* Latest Blog Posts and Recipe Categories Sections */}
+      <section className="py-16 bg-g">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8">Featured Recipes</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="bg-gray-200 py-64">
+              <h2 className="text-3xl text-center font-bold mb-8">
+                Latest Blog Posts
+              </h2>
+            </div>
+            <div className="bg-gray-200 py-64">
+              <h2 className="text-3xl text-center font-bold mb-8">
+                Recipe Categories
+              </h2>
+            </div>
+          </div>
         </div>
       </section>
 
-      {/* Latest Blog Posts Section */}
-      <section className=" py-16">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8">Latest Blog Posts</h2>
-        </div>
-      </section>
-
-      {/* Recipe Categories Section */}
-      <section className=" py-16">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8">Recipe Categories</h2>
-        </div>
-      </section>
-
-      {/* About us Categories Section */}
-      <section className="py-16">
+      {/* About us Section */}
+      <section className="py-64">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8">About us</h2>
-        </div>
-      </section>
-
-      {/* Newslater us Categories Section */}
-      <section className="bg-secondary py-16">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8">
-            Subscribe to our newslater
-          </h2>
         </div>
       </section>
     </div>
