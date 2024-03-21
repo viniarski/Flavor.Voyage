6657b9b9b5ccc9698a61c7c14a425a6d2001254d
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-03-21 16:54:25 +0000

    header and sign-in-up  styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,72 +1,66 @@
-import Link from "next/link";
-import Image from "next/image";
-import logo from "../../public/logo.png";
-import { UserButton } from "@clerk/nextjs";
-import { auth, currentUser } from "@clerk/nextjs";
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../public/logo.png';
+import { UserButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs';
+import loginIcon from '../../public/icons/login.png';
 
 export default function Header() {
   const { userId } = auth();
 
   return (
     <header className="flex bg-white py-4 px-8 justify-between items-center">
-      <div className="flex">
-        <Image src={logo} height={100} width={80} alt="Flavor.Voyage Logo" />
-        <h1 className="text-3xl font-bold p-4">Flavor.Voyage</h1>
-      </div>
+      <Link href="/">
+        <div className="flex items-center">
+          <Image src={logo} height={100} width={80} alt="Flavor.Voyage Logo" />
+          <h1 className="text-3xl font-bold p-4">Flavor.Voyage</h1>
+        </div>
+      </Link>
       <nav>
-        <ul className="flex gap-8 text-2xl ">
-          {userId ? (
-            <UserButton />
-          ) : (
-            <>
-              <Link
-                href={"/sign-in"}
-                className="hover:underline hover:underline-offset-8 hover:decoration-accent"
-              >
-                Sign In
-              </Link>
-              <Link
-                href={"/sign-up"}
-                className="hover:underline hover:underline-offset-8 hover:decoration-accent"
-              >
-                Sign Up
-              </Link>
-            </>
-          )}
-
+        <ul className="flex gap-8 text-2xl items-center">
           <Link
             href={'/'}
-
             className="hover:underline hover:underline-offset-8 hover:decoration-accent"
           >
             Home
           </Link>
           <Link
-
             href={'/recipes'}
-
             className="hover:underline hover:underline-offset-8 hover:decoration-accent"
           >
             Recipes
           </Link>
           <Link
-            href={"#"}
+            href={'#'}
             className="hover:underline hover:underline-offset-8 hover:decoration-accent"
           >
             About
           </Link>
           <Link
-            href={"/blog-posts"}
+            href={'/blog-posts'}
             className="hover:underline hover:underline-offset-8 hover:decoration-accent"
           >
             Blog
           </Link>
           <Link
-            href={"#"}
+            href={'#'}
             className="hover:underline hover:underline-offset-8 hover:decoration-accent"
           >
             Contact
           </Link>
+          {userId ? (
+            <UserButton />
+          ) : (
+            <Link href={'/sign-in'}>
+              <Image
+                src={loginIcon}
+                height={24}
+                width={24}
+                alt="Sign In"
+                className="ml-4"
+              />
+            </Link>
+          )}
         </ul>
       </nav>
     </header>
