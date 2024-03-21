41e9927c0c87f743c768718a69089cfcb0fbe541
Loren Rivers <loren.rivers@hotmail.co.uk>
2024-03-21 16:51:17 +0000

    incorporated PageHeader component to all blog posts page

--- src/app/blog-posts/page.js
+++ src/app/blog-posts/page.js
@@ -1,9 +1,14 @@
 import BlogPostList from "@/components/BlogPostList";
+import PageHeader from "@/components/pageHeader";
 
 export default async function BlogPostsPage() {
   return (
     <div>
-      <h2 className="text-5xl font-bold mb-4 text-center p-3">Blogs</h2>
+      <PageHeader
+        header={"Blogs"}
+        description={"Check out our latest blogs"}
+        img={"url('/images/4.avif')"}
+      />
       <BlogPostList />
     </div>
   );
