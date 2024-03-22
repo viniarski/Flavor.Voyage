9b6e44a4c8da674263026d06120e298e975d1e51
Maurot11 <mauro.co.uk@hotmail.com>
2024-03-22 10:38:19 +0000

    structure complete, trying to get username from users table

--- src/app/recipes/[recipe_id]/page.js
+++ src/app/recipes/[recipe_id]/page.js
@@ -4,6 +4,8 @@ import PageHeader from "@/components/pageHeader"
 import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import time from "../../../../public/icons/duration.png"
+import serving from "../../../../public/icons/serving_size.png"
+import Image from "next/image";
 
 export default function Page({params}) {
 
@@ -23,40 +25,53 @@ export default function Page({params}) {
                 .select('*')
                 .eq('recipe_id', `${params.recipe_id}`);
 
-                // console.log(data[0])
+            // console.log(data[0])
             setRecipes(data[0])
         };
     
         fetchRecipes();
     }, []);
 
+    // console.log(recipes)
+
     return (
         <div className="min-h-full flex flex-col items-center">
             <PageHeader header={`${recipes.recipe_title}`} description={`How to prepare and cook ${recipes.recipe_title}`} img={"url('/images/4.avif')"} />
-            <div className="grid grid-cols-2 p-16 gap-16">
-            <img src={recipes.imgurl} className="rounded-3xl" alt="Chickpea" width={400} />
-                <div>
-                    <h2>Category: {recipes.category}</h2>
-                    <div className="grid grid-cols-2 max-h-8">
-                        <div className="flex">
-                            <img src="url('../../../../public/icons/duration.png')" alt="duration" width={100} height={100} />
-                            <p>{recipes.preparation_time} Minutes</p>
-                        </div>
-                        <div className="flex">
-                            <img src="url('../../../../public/icons/duration.png')" alt="duration" width={100} height={100} />
-                            <p>Serves {recipes.serving_size}</p>
-                        </div>
-                    </div>
+            <div className="py-4 flex flex-col items-center">
+                <div className="grid grid-cols-2 p-16 gap-16">
+                <img src={recipes.imgurl} className="rounded-3xl" alt="Chickpea" width={400} />
                     <div>
-                        <h3>Ingredients:</h3>
-                        {recipes.recipe_ingredients.map((ingredient, i) => (
-                            <li key={i} className="text-gray-700">
-                            {ingredient}
-                            </li>
-                        ))}
+                        <h2 className="text-lg font-bold mb-2 text-accent">Category: {recipes.category}</h2>
+                        <div className="grid grid-cols-2 max-h-8 my-2">
+                            <div className="flex gap-2">
+                                <Image src={time} alt="duration" width={25} height={10} />
+                                <p>{recipes.preparation_time} Minutes</p>
+                            </div>
+                            <div className="flex gap-2">
+                                <Image src={serving} alt="duration" width={25} height={10} />
+                                <p>Serves {recipes.serving_size}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className="text-lg font-bold mb-2 text-accent">Ingredients:</h3>
+                            {recipes.recipe_ingredients.map((ingredient, i) => (
+                                <li key={i} className="text-gray-700">
+                                {ingredient}
+                                </li>
+                            ))}
+                        </div>
                     </div>
                 </div>
+                <div className="max-w-[850px]">
+                    <h3 className="text-lg font-bold mb-2 text-accent">Cooking Instruction:</h3>
+                    <p className="text-lg">{recipes.cooking_instructions}</p>
+                </div>
+                <div className="my-4">
+                    <p>Uploaded: {recipes.date_created}</p>
+                    <p>By: {recipes.user_id}</p>
+                </div>
             </div>
+            
         </div>
     )
 }
