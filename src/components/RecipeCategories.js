c46f4e3acc9d7539c5060b748aa809779f5d0e7b
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-03-23 02:03:29 +0000

    styling updates

--- src/components/RecipeCategories.js
+++ src/components/RecipeCategories.js
@@ -1,6 +1,7 @@
 // components/RecipeCategories.js
 'use client';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function RecipeCategories() {
   const categories = [
@@ -15,12 +16,32 @@ export default function RecipeCategories() {
   ];
 
   return (
-    <div className="bg-gray-100 p-10 rounded-lg">
-      <div className="grid grid-cols-4 gap-16">
+    <div className="bg-gray-100 p-8 rounded-lg">
+      <div className="grid grid-cols-4 gap-8">
         {categories.map((category) => (
           <Link key={category} href={`/recipes?category=${category}`}>
-            <div className="bg-white rounded-full h-32 w-32 flex items-center justify-center shadow">
-              <span className="text-lg font-bold">{category}</span>
+            <div className="flex flex-col items-center">
+              <div className="bg-white rounded-full h-32 w-32 flex items-center justify-center shadow relative overflow-hidden">
+                {category === 'Desserts' && (
+                  <Image
+                    src="/images/6.avif"
+                    alt="Dessert"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                )}
+                {category === 'Vegetarian' && (
+                  <Image
+                    src="/images/8.avif"
+                    alt="Vege"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                )}
+              </div>
+              <span className="text-lg font-bold mt-2">{category}</span>
             </div>
           </Link>
         ))}
